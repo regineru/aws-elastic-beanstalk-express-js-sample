b71e2a33c0da3d094aeed1c79f5e010ecd5acfa8
Regine Ruud <regineruud@Regines-MacBook-Pro-2.local>
2022-08-24 14:18:49 +0200

    full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Regine wrote this'));
+app.get('/', (req, res) => res.send('Regine wrote this new message after adding review'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
